92372d24fe6ec3e1e69b09b6c686defc46e58b12
Renil Soni <srenil063@gmail.com>
2023-10-29 09:10:39 -0400

    Initial Commit

---
+++ Assignment_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_2
+//
+//  Created by Raj-Renil on 2023-10-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
